b4fec5ad9b7b89694abac7c9f0dfbccd5c573f57
Songyi Kim <ksl2950@o.cnu.ac.kr>
2024-01-16 01:11:36 +0900

    setting: spring + 멀티 모듈 환경 세팅

---
+++ domain/src/test/java/nexters/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package nexters.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
