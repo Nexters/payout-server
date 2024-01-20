c6523c4ebfc8e8ebddf7e652567ae4f5213bd6b7
Songyi Kim <ksl2950@o.cnu.ac.kr>
2024-01-20 20:03:53 +0900

    refactor: convert pascal to upper case

--- domain/src/main/java/nexters/domain/stock/Sector.java
+++ domain/src/main/java/nexters/domain/stock/Sector.java
@@ -1,16 +1,22 @@
 package nexters.domain.stock;
 
 public enum Sector {
-    Technology,
-    CommunicationServices,
-    Healthcare,
-    ConsumerCyclical,
-    ConsumerDefensive,
-    BasicMaterials,
-    FinancialServices,
-    Industrials,
-    RealEstate,
-    Energy,
-    Utilities,
-    ETC
+    TECHNOLOGY("Technology"),
+    COMMUNICATION_SERVICES("CommunicationServices"),
+    HEALTHCARE("Healthcare"),
+    CONSUMER_CYCLICAL("ConsumerCyclical"),
+    CONSUMER_DEFENSIVE("ConsumerDefensive"),
+    BASIC_MATERIALS("BasicMaterials"),
+    FINANCIAL_SERVICES("FinancialServices"),
+    INDUSTRIALS("Industrials"),
+    REAL_ESTATE("RealEstate"),
+    ENERGY("Energy"),
+    UTILITIES("Utilities"),
+    ETC("ETC");
+
+    private final String value;
+
+    Sector(final String value) {
+        this.value = value;
+    }
 }
