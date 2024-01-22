537069f472f2d66f3a256164389ef7161c6b13c8
Songyi Kim <ksl2950@o.cnu.ac.kr>
2024-01-22 23:54:45 +0900

    feat: add custom exception

---
+++ core/src/main/java/nexters/dividend/core/exception/error/NotFoundException.java
@@ -0,0 +1,7 @@
+package nexters.dividend.core.exception.error;
+
+public class NotFoundException extends BaseException {
+    public NotFoundException(final String message) {
+        super(message);
+    }
+}
