ba38f83ec9627479d2d4979bc604da9a9ba26816
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-01-25 16:17:16 +0900

    setting: add scheduling annotation and scan packages

--- dividend-batch/src/main/java/nexters/dividendbatch/DividendBatchApplication.java
+++ dividend-batch/src/main/java/nexters/dividendbatch/DividendBatchApplication.java
@@ -2,8 +2,10 @@ package nexters.dividendbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = { "nexters.dividendbatch", "nexters.domain" })
 public class DividendBatchApplication {
 
     public static void main(String[] args) {
