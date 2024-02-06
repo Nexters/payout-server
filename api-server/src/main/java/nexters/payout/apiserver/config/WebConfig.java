b3b9050a67c15ae715eb1d5663ecc9975d063eab
Songyi Kim <ksl2950@o.cnu.ac.kr>
2024-02-07 02:15:47 +0900

    feat: add web config (cors)

---
+++ api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
@@ -0,0 +1,18 @@
+package nexters.payout.apiserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
