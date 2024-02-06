fd8cf693a2db7c94f97d974603dc069574f9ea1f
Songyi Kim <ksl2950@o.cnu.ac.kr>
2024-02-07 02:11:54 +0900

    feat: implement controller

--- api-server/src/main/java/nexters/payout/apiserver/PayoutApiServerApplication.java
+++ api-server/src/main/java/nexters/payout/apiserver/PayoutApiServerApplication.java
@@ -3,7 +3,11 @@ package nexters.payout.apiserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"nexters.payout.core",
+		"nexters.payout.domain",
+		"nexters.payout.apiserver"
+})
 public class PayoutApiServerApplication {
 
 	public static void main(String[] args) {
---
+++ api-server/src/main/java/nexters/payout/apiserver/stock/presentation/StockController.java
@@ -0,0 +1,33 @@
+package nexters.payout.apiserver.stock.presentation;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import nexters.payout.apiserver.stock.application.StockService;
+import nexters.payout.apiserver.stock.application.dto.response.SectorRatioResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/stocks")
+public class StockController {
+
+    private final StockService stockService;
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "SUCCESS"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST")
+    })
+    @GetMapping("/sector-ratio")
+    public ResponseEntity<List<SectorRatioResponse>> findSectorRatios(
+            @Size(min = 1) @RequestParam List<String> tickers) {
+        return ResponseEntity.ok(stockService.findSectorRatios(tickers));
+    }
+}
