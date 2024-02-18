8597bb410d79d3322f71ed59c88db6eb74ca8a9f
Songyi Kim <52441906+songyi00@users.noreply.github.com>
2024-02-18 19:36:37 +0900

    feat: update swagger content (#32)
    * feat: update swagger and add interface
    
    * feat: update stock detail response

--- api-server/src/main/java/nexters/payout/apiserver/stock/presentation/StockController.java
+++ api-server/src/main/java/nexters/payout/apiserver/stock/presentation/StockController.java
@@ -1,5 +1,7 @@
 package nexters.payout.apiserver.stock.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -19,34 +21,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/stocks")
-public class StockController {
+public class StockController implements StockControllerDocs {
 
     private final StockQueryService stockQueryService;
 
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "SUCCESS"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "500", description = "SERVER ERROR",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})
-    })
     @GetMapping("/{ticker}")
-    public ResponseEntity<StockDetailResponse> getStockByTicker(@PathVariable String ticker) {
+    public ResponseEntity<StockDetailResponse> getStockByTicker(
+            @Parameter(description = "ticker name of stock", example = "AAPL")
+            @PathVariable String ticker
+    ) {
         return ResponseEntity.ok(stockQueryService.getStockByTicker(ticker));
     }
 
 
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "SUCCESS"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "500", description = "SERVER ERROR",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})
-    })
     @PostMapping("/sector-ratio")
     public ResponseEntity<List<SectorRatioResponse>> findSectorRatios(
             @Valid @RequestBody final SectorRatioRequest request) {
