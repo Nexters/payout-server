bed56a716cf8691f07c9ed4464be899b4273ec51
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-02-19 23:21:27 +0900

    refactor: refactor swagger config

--- api-server/src/main/java/nexters/payout/apiserver/dividend/presentation/DividendController.java
+++ api-server/src/main/java/nexters/payout/apiserver/dividend/presentation/DividendController.java
@@ -1,9 +1,5 @@
 package nexters.payout.apiserver.dividend.presentation;
 
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +7,6 @@ import nexters.payout.apiserver.dividend.application.DividendQueryService;
 import nexters.payout.apiserver.dividend.application.dto.request.DividendRequest;
 import nexters.payout.apiserver.dividend.application.dto.response.MonthlyDividendResponse;
 import nexters.payout.apiserver.dividend.application.dto.response.YearlyDividendResponse;
-import nexters.payout.core.exception.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,39 +24,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/dividends")
-public class DividendController {
+public class DividendController implements DividendControllerDocs {
 
     private final DividendQueryService dividendQueryService;
 
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "SUCCESS"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "500", description = "SERVER ERROR",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})
-    })
     @PostMapping("/monthly")
     public ResponseEntity<List<MonthlyDividendResponse>> getMonthlyDividends(@RequestBody @Valid DividendRequest request) {
 
         return ResponseEntity.ok(dividendQueryService.getMonthlyDividends(request));
     }
 
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "SUCCESS"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "500", description = "SERVER ERROR",
-                    content = {@Content(schema = @Schema(implementation = ErrorResponse.class))})
-    })
     @PostMapping("/yearly")
     public ResponseEntity<YearlyDividendResponse> getYearlyDividends(@RequestBody @Valid DividendRequest request) {
 
         return ResponseEntity.ok(dividendQueryService.getYearlyDividends(request));
     }
-
-
 }
