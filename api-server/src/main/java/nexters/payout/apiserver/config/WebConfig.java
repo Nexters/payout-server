406b349b2c702bffa016af8f6cffc63d3f72149c
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-02-25 05:31:27 +0900

    fix: fix cors options

--- api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
+++ api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
