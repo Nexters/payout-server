a87ec20ea06682454e60ac2874de41530f3e4e5a
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-02-25 04:17:15 +0900

    fix: add cors option

--- api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
+++ api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
