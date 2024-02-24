b292f206a75d86c278bd85eb2cab24cd13d3d520
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-02-25 04:23:05 +0900

    fix: add cors option

--- api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
+++ api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -15,4 +16,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-}
+}*/
