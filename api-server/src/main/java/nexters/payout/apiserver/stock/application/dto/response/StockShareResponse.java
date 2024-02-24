684b4fd716a36344c41dfe1875c2b183c1ebc9eb
Songyi Kim <ksl2950@o.cnu.ac.kr>
2024-02-25 05:04:27 +0900

    feat: add swagger option

--- api-server/src/main/java/nexters/payout/apiserver/stock/application/dto/response/StockShareResponse.java
+++ api-server/src/main/java/nexters/payout/apiserver/stock/application/dto/response/StockShareResponse.java
@@ -1,9 +1,12 @@
 package nexters.payout.apiserver.stock.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import nexters.payout.domain.stock.domain.service.SectorAnalysisService.StockShare;
 
 public record StockShareResponse(
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "stock response")
         StockResponse stockResponse,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "share")
         Integer share
 ) {
 
