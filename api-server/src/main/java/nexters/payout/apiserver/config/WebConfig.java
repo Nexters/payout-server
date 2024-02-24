25d8f6d7f469a5e707cb6b5a70fecdda544648a6
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-02-25 00:59:04 +0900

    fix: fix nignx.conf

--- api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
+++ api-server/src/main/java/nexters/payout/apiserver/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
