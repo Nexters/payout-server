3543ad2101534ca6f62c7f9408d3b9cc7684bfc0
Songyi Kim <ksl2950@o.cnu.ac.kr>
2024-02-26 00:19:04 +0900

    chore: remove unused code

--- deploy.sh
+++ deploy.sh
@@ -19,55 +19,5 @@ sleep 10
 sed -it "s/$OTHER_SERVICE/$TARGET_SERVICE/" $NGINX_CONF
 docker-compose -f /home/docker-compose.yml restart nginx
 
-#docker exec $NGINX_CONTAINER nginx -s reload
-#docker-compose -f /home/docker-compose.yml exec nginx service nginx reload
-
 # Stop the other service
 docker-compose -f /home/docker-compose.yml stop $OTHER_SERVICE
-
-
-##!/bin/bash
-#
-#NGINX_CONF="/home/nginx.conf"
-#NGINX_CONTAINER="nginx"
-#GREEN_API_CONTAINER="green-api"
-#BLUE_API_CONTAINER="blue-api"
-#BATCH_CONTAINER="batch"
-#
-## nginx 정상 동작 확인
-#IS_NGINX_RUNNING=$(docker ps | grep ${NGINX_CONTAINER})
-#
-## api-server 정상 동작 확인
-#IS_BLUE_RUNNING=$(cat $NGINX_CONF | grep $BLUE_API_CONTAINER)
-#
-#if [ -z "$IS_NGINX_RUNNING" ]; then
-#  # 정상 작동하지 않을 시 nginx 재시작
-#  echo "nginx container is not running. run nginx container"
-#  docker rmi nginx
-#  docker-compose -f /home/docker-compose.yml up -d nginx
-#else
-#  echo "nginx is already running"
-#fi
-#
-#if [ -z "$IS_BLUE_RUNNING" ]; then
-#    TARGET_SERVICE=${BLUE_API_CONTAINER}
-#    OTHER_SERVICE=${GREEN_API_CONTAINER}
-#else
-#    TARGET_SERVICE=${GREEN_API_CONTAINER}
-#    OTHER_SERVICE=${BLUE_API_CONTAINER}
-#fi
-#  sleep 3
-#
-#echo "Switching to $TARGET_SERVICE..."
-#
-#docker-compose -f /home/docker-compose.yml up -d $TARGET_SERVICE $BATCH_CONTAINER
-#
-## Nginx 설정 업데이트하여 트래픽 전환
-#sed -it "s/$OTHER_SERVICE/$TARGET_SERVICE/" $NGINX_CONF
-#docker exec $NGINX_CONTAINER nginx -s reload
-#
-##docker stop $OTHER_SERVICE
-#
-## Nginx 설정 적용을 위해 Nginx 프로세스 재로드
-#
-#echo "$TARGET_SERVICE deployment completed."
