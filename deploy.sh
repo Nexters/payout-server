5a1f0711432114de3b3390c64f1c53b1064421d5
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-02-27 10:17:05 +0900

    setting: add docker/docker-compose install script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
+# Docker install
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed..."
+    echo "Docker install start..."
+    sudo apt-get update -y
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update -y
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    echo "Docker install complete"
+else
+    echo "Docker is already installed"
+fi
+
+# Docker-compose install
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker-compose is not installed..."
+    echo "Docker-compose install start..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    echo "Docker-compose install complete!"
+else
+    echo "Docker-compose is already installed"
+fi
+
 RUNNING_CONTAINER=$(docker ps | grep blue)
 NGINX_CONF="/home/nginx.conf"
 RUNNING_NGINX=$(docker ps | grep nginx)
