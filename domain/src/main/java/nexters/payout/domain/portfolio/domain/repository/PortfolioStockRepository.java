a5d0b8c0c81a9c649dfc94e04ebc331268c7cf13
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-03-30 15:16:31 +0900

    feat: add portfolio, portfolio stock repository

---
+++ domain/src/main/java/nexters/payout/domain/portfolio/domain/repository/PortfolioStockRepository.java
@@ -0,0 +1,11 @@
+package nexters.payout.domain.portfolio.domain.repository;
+
+import nexters.payout.domain.portfolio.PortfolioStock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PortfolioStockRepository extends JpaRepository<PortfolioStock, UUID> {
+    List<PortfolioStock> findAllByPortfolioId(UUID portfolioId);
+}
