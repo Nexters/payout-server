a9e5567bfc8e3c048822b612a1ebeb06d9ef0ff8
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2024-04-16 14:05:25 +0900

    test: add portfolio controller test

---
+++ api-server/src/test/java/nexters/payout/apiserver/dividend/common/IntegrationTest.java
@@ -0,0 +1,36 @@
+package nexters.payout.apiserver.dividend.common;
+
+import io.restassured.RestAssured;
+import nexters.payout.domain.dividend.domain.repository.DividendRepository;
+import nexters.payout.domain.stock.domain.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public abstract class IntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    public StockRepository stockRepository;
+
+    @Autowired
+    public DividendRepository dividendRepository;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @AfterEach
+    void afterEach() {
+        dividendRepository.deleteAll();
+        stockRepository.deleteAll();
+    }
+}
